7915c5ffdbb8a87fed1eb62e199fc0b49f199b3a
Zachdehooge <Zacharydehooge@gmail.com>
2025-07-24 09:10:04 -0400

    Refactor

--- src/main.zig
+++ src/main.zig
@@ -1,12 +1,12 @@
 const std = @import("std");
 
 // Sample function to demonstrate a simple loop and timer usage
-fn foo() !void {
+fn foo(iterations: u8) !void {
     var x: u8 = 1;
     var timer: std.time.Timer = try std.time.Timer.start();
     const time_from_start_to_read_ns: u64 = timer.read();
 
-    while (x <= 100) : (x += 1) {
+    while (x <= iterations) : (x += 1) {
         std.debug.print("x = {}\n", .{x});
     }
 
@@ -15,11 +15,11 @@ fn foo() !void {
 
 // Error handling example with a custom error type
 const less_than_10 = error{
-    less_than_10,
+    equal_to_0,
 };
 fn bar(x: u8) !u8 {
-    if (x <= 9) {
-        return error.less_than_10;
+    if (x <= 0) {
+        return error.equal_to_0;
     } else {
         return x;
     }
@@ -29,19 +29,24 @@ fn bar(x: u8) !u8 {
 pub fn main() !void {
     var buffer: [100]u8 = undefined;
     const stdin = std.io.getStdIn().reader();
-    const stdout = std.io.getStdOut().writer();
+
+    std.debug.print("\n", .{});
 
     std.debug.print("Enter a number: ", .{});
 
     const input_slice = try stdin.readUntilDelimiter(&buffer, '\n');
 
     const input_number = try std.fmt.parseInt(u8, input_slice, 10);
-    try stdout.print("The user entered: {}\n", .{input_number});
 
     const bars = bar(input_number) catch |err| {
         std.debug.print("{}\n", .{err});
         return;
     };
 
-    std.debug.print("x = {}\n", .{bars});
+    std.debug.print("\n", .{});
+
+    foo(bars) catch |err| {
+        std.debug.print("Error in foo: {}\n", .{err});
+        return;
+    };
 }
